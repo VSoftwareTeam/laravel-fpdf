28fbd31c026bfee52170853d1106c99b00e79e47
dave-antrobus <40212601+dave-antrobus@users.noreply.github.com>
2020-10-19 14:23:26 +0100

    Update Fpdf.php

--- src/Fpdf/Fpdf.php
+++ src/Fpdf/Fpdf.php
@@ -7,7 +7,7 @@
 * Author:  Olivier PLATHEY                                                     *
 *******************************************************************************/
 
-define('FPDF_VERSION','1.82');
+if(!defined('FPDF_VERSION')) define('FPDF_VERSION','1.82');
 
 class Fpdf
 {
